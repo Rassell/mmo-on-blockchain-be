0cfc384c820a67bfae8c77095ebb9d4336bf1cd1
Gonzalo Soriano <sordesos@gmail.com>
2022-04-26 21:43:50 +0200

    trying to deploy on gcp

---
+++ terraform/arena.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  project = "mmo-on-blockchain"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-omn-prd"
+    prefix = "terraform/state"
+  }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./path/to/zip/file/which/contains/code"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "func-arena"
+  description = "My function"
+  runtime     = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloHttp"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
