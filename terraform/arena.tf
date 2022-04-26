08c6da99fefb22763c3d8d357174fc458840754f
Gonzalo Soriano <sordesos@gmail.com>
2022-04-26 21:53:56 +0200

    fix deploy?

--- terraform/arena.tf
+++ terraform/arena.tf
@@ -11,25 +11,12 @@ terraform {
   }
 }
 
-resource "google_storage_bucket" "bucket" {
-  name     = "mmo-on-blockchain-bucket"
-  location = "EU"
-}
-
-resource "google_storage_bucket_object" "archive" {
-  name   = "index.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "./path/to/zip/file/which/contains/code"
-}
-
 resource "google_cloudfunctions_function" "function" {
   name        = "func-arena"
   description = "My function"
   runtime     = "nodejs16"
 
   available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.archive.name
   trigger_http          = true
   entry_point           = "helloHttp"
 }
