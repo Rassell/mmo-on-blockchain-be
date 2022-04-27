54d2b9581d945147794df5ad2beb48214e9b6aaa
Gonzalo Soriano <sordesos@gmail.com>
2022-04-27 23:39:20 +0200

    wip for blockchain communication

--- functions/arena/__tests__/index.spec.ts
+++ functions/arena/__tests__/index.spec.ts
@@ -2,23 +2,22 @@ import { Request, Response } from '@google-cloud/functions-framework';
 
 import { helloHttp } from '../index';
 
-describe('init', () => {
+describe('addChampionToArena', () => {
   const mockRequest = jest.fn<Request, []>();
   const mockResponse = jest.fn<Response, []>(() => ({
     send: jest.fn(),
-    
-  }));
+  } as any));
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should return a string', () => {
+  it('should return an id', async () => {
     const req = new mockRequest();
     const res = new mockResponse();
 
-    helloHttp(req, res);
+    await helloHttp(req, res);
 
-    expect(res.send).toHaveBeenCalledWith('Hello World!');
+    expect(res.send).toHaveBeenCalledWith(expect.any(String));
   });
 });
--- functions/arena/index.ts
+++ functions/arena/index.ts
@@ -1,5 +1,30 @@
-import { Request, Response } from '@google-cloud/functions-framework';
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv').config();
+}
 
-export function helloHttp(req: Request, res: Response) {
-  res.send('Hello World!');
+import { Request, Response } from '@google-cloud/functions-framework';
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+
+import { championFactory, roster } from './assets';
+
+const rosterContractAddress = '0xC431f0678aDec03906dD0f688102653cbbE77259';
+const championContractAddress = '0x885cbD739bf5A87CEb7eCF3dfa7193c4628974a6';
+
+const web3 = createAlchemyWeb3(process.env.ALCHEMY_KEY);
+const rosterContract = new web3.eth.Contract(
+  roster.abi as any,
+  rosterContractAddress,
+);
+const championContract = new web3.eth.Contract(
+  championFactory.abi as any,
+  championContractAddress,
+);
+
+export async function helloHttp(req: Request, res: Response) {
+  const response = await web3.eth.getBalance(
+    '0xd5a604Cf08a18E5d811499B3EB1908172386e51e',
+  );
+
+  res.send(response);
 }
