e94c1889213ea7014368374081d62c802955db39
Gonzalo Soriano <sordesos@gmail.com>
2022-04-27 23:38:35 +0200

    formatting

--- functions/arena/esbuild.js
+++ functions/arena/esbuild.js
@@ -1,8 +1,10 @@
-require('esbuild')
-  .build({
-    entryPoints: ['index.ts'],
-    bundle: true,
-    outfile: 'dist/index.js',
-    platform: 'node',
-  })
-  .catch(() => process.exit(1));
+// eslint-disable-next-line
+const { build } = require('esbuild');
+
+build({
+  entryPoints: ['index.ts'],
+  bundle: true,
+  outfile: 'dist/index.js',
+  platform: 'node',
+  external: ['electron'],
+}).catch(() => process.exit(1));
