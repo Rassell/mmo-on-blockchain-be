5183dafa45ce7722648a034cb3bf929bede0e3d0
Gonzalo Soriano <sordesos@gmail.com>
2022-04-27 23:38:55 +0200

    contracts

---
+++ functions/arena/assets/index.ts
@@ -0,0 +1,3 @@
+export { default as roster } from "./Roster.json";
+export { default as bossFactory } from "./BossFactory.json";
+export { default as championFactory } from "./ChampionFactory.json";
